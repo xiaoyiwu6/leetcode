ba9b36d960f235305984fbf843d9161b5cef1b39
leo <xiaoyiwu6@gmail.com>
2021-03-10 17:25:24 +0800

    剑指13

--- stoffer.h
+++ stoffer.h
@@ -9,10 +9,14 @@ class Solution {
 public:
     //剑指 Offer 12. 矩阵中的路径
     bool exist(vector<vector<char>>&,string);
+    //剑指 Offer 13. 机器人的运动范围
+    int movingCount(int,int,int);
 private:
     int rows,cols;
     //深度优先
     bool dfs(vector<vector<char>>&,string,int,int,int);
+    //判断位数之和是否大于k
+    bool sum_compare(int,int,int);
 };
 
 #endif 
